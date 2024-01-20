c6a3f141ae52db46eef6af13eba9ac1f99c67a54
mario <mcalvarr@ucm.es>
2024-01-21 00:12:16 +0100

    Primera versión con lectura por fichero

--- entregable.hs
+++ entregable.hs
@@ -50,7 +50,7 @@ evalBool ((:||) e1 e2) est = evalBool e1 est || evalBool e2 est
 
 --- Código para el programa en sí ---
 infixl 1 :=     -- Reducimos la prioridad de la asignación a casi el mínimo para separar la variable de la expresión adecuadamente
-data Instruccion = Variable := Expr | Cond BoolExpr Programa Programa | While BoolExpr Programa deriving Show
+data Instruccion = Variable := Expr | Cond BoolExpr Programa Programa | While BoolExpr Programa deriving (Show, Read)
 type Programa = [Instruccion]
 
 -- Función auxiliar para modificar el estado del programa
@@ -95,50 +95,73 @@ s0 = [("X", 3)]
 leerEstado::String -> Estado
 leerEstado = read
 
+leerExpr::String -> Expr
+leerExpr = read
+
+leerBoolExpr::String -> BoolExpr
+leerBoolExpr = read
+
 -- Para leer una asignación:
 -- · La primera palabra es el nombre de la variable
 -- · Las dos primeras palabras son el nombre y el signo de la variable
 -- · El resto lo leemos como una expresión
+-- leerAsig::String -> Instruccion
+-- leerAsig s = head (words s) := leerExpr (concat $ drop 2 $ words s)
+
 leerAsig::String -> Instruccion
-leerAsig s = head (words s) := read (concat $ drop 2 $ words s)
+leerAsig = read
 
-readProgAux::Handle -> Programa -> Programa
-readProgAux h _ = []
+leerWhile::String -> Handle -> IO Instruccion
+leerWhile line h = do 
+    let cond = leerBoolExpr $ unwords $ tail $ words line
+    _ <- hGetLine h     -- Saltamos la línea que abre corchete
+    prog <- leerProgramaAux h []
+    return $ While cond prog
 
-leerWhile::String -> Handle -> Instruccion
-leerWhile line h = While cond prog
-    where cond = read $ concat $ drop 1 $ words line --La primera palabra de la linea es "While"
-          prog = readProgAux h []
-
-leerCond::String -> Handle -> Instruccion
-leerCond line h = Cond cond prog1 prog2
-    where cond = read $ concat $ drop 1 $ words line --La primera palabra de la linea es "While"
-          prog1 = readProgAux h []
-          prog2 = readProgAux h []
+leerCond::String -> Handle -> IO Instruccion
+leerCond line h = do 
+    let cond = leerBoolExpr $ unwords $ tail $ words line
+    _ <- hGetLine h     -- Saltamos la línea que abre corchete
+    prog1 <- leerProgramaAux h []
+    _ <- hGetLine h     -- Saltamos la línea que abre corchete
+    prog2 <- leerProgramaAux h []
+    return $ Cond cond prog1 prog2
 
 -- Suposiciones del programa:
 -- 1) En cada línea tenemos una instrucción
 -- 2) Las instrucciones condicionales y de bucle tienen el siguiente formato
---      · Una primera línea con el identificado (Cond / While) y, entre
+--      · Una primera línea con el identificado (Cond / While) y, SIN
 --        paréntesis, la expresión booleana
---      · La siguiente línea, directamente, la primera instrucción del programa que
---        ejecutan si se cumple la condición, seguido del resto de instrucciones (una
+--      · La siguiente línea será un corchete que abre "[" y la última, uno que
+--        cierra "]"
+--      · Tras esto, la primera instrucción del programa que se
+--        ejecuta si se cumple la condición, seguido del resto de instrucciones (una
 --        por línea)
---      · La primera línea empezará con un corchete "[" y la última, con uno que
---        cierra "]".
---      · En el caso del cond, la misma sintaxis que para el programa 1
+--      · En el caso del Cond, la misma sintaxis que para el programa 1 para el 2
+--  (Adjunto aparece un fichero con el programa factorial como ejemplo)
 leerProgramaAux::Handle -> Programa -> IO Programa
 leerProgramaAux h p = do
-    line <- hGetLine h
-    let fw = head $ words line
-    if null line
-        then return $ reverse p
+    eof <- hIsEOF h
+    if eof then
+        return $ reverse p
     else do
-        p' <- case fw of
-            "While" -> return (leerWhile line h : p)
-            "Cond"  -> return (leerCond line h : p)
-            _       -> return (leerAsig line : p) -- Le damos la vuelta a todo al final
-        leerProgramaAux h p'
+        line <- hGetLine h
+        let fw = head $ words line
+        if fw == "]" then
+            return $ reverse p         -- Corchete que cierra -> Acaba la lectura del programa
+        else do
+            p' <- case fw of
+                "While" -> do
+                    while <- leerWhile line h
+                    return (while : p)
+
+                "Cond"  -> do
+                    cond <- leerCond line h
+                    return (cond : p)
+
+                _       -> return (leerAsig line : p) -- Le damos la vuelta a todo al final
+
+            leerProgramaAux h p'
 
 
 leerPrograma::Handle -> IO Programa
@@ -147,10 +170,12 @@ leerPrograma h = leerProgramaAux h []
 main = do
     print "Introduzca el nombre del fichero del programa"
     fileName <- getLine
-    handle <- openFile fileName ReadMode
+    handle <- openFile "factorial.inv" ReadMode
     print "Introduzca el estado inicial (El estado es una lista de parejas en la que el primer elemento es una cadena de caracteres que representa el nombre de una variable y el segundo un entero que indica el valor de dicha variable)"
     cadenaEstadoInicial <- getLine
     let estadoInicial = leerEstado cadenaEstadoInicial
+    let estadoInicial = [("X", 3)]
     programa <- leerPrograma handle
+    print $ ejecuta programa estadoInicial
 
     hClose handle
